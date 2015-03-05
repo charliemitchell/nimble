4b8a40c9724a92e0b2fb3506615a9602f6099d17
charles mitchell <charliesemailis@gmail.com>
2015-03-05 12:14:47 -0800

    Typo

--- blueprint/model.js
+++ blueprint/model.js
@@ -24,7 +24,7 @@ var {{model}} = new Schema({
     If you don't want to use mongoose's default behavior for determining collection names
     you can specify the exact name of your collection by using this instead.
     switch to this ----> mongoose.model('{{model}}', {{model}}, '{{model}}');
-    Otherwise mongoose will use follow REST and pluralize what it thinks a resource is.
+    Otherwise mongoose will follow REST resource conventions and pluralize what it thinks a resource is.
     So If your model was "Customer" then it would create a resource (collection) "Customers". 
     By Specifying the 3rd paramater, you are telling mongoose that you would like to force a specific collection name.
 */
